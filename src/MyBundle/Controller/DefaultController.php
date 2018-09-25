da53b54f53653a8fddba8c77729d6f0f997638b9
PANDA-PC <yassinkisrawi42@gmail.com>
2018-09-25 09:51:35 +0100

    first time

---
+++ src/MyBundle/Controller/DefaultController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace MyBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/todo")
+     */
+    public function indexAction()
+    {
+        return $this->render('MyView/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+    /**
+     *
+     * */
+    public function premierAction()
+    {
+        // Contenu du contrôleur à préparer
+    }
+
+    /**
+     * @Route("/tobe")
+     */
+    public function todoGetAction(){
+        return $this->render('MyView/todo.html.twig',[
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+}
